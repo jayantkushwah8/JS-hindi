2a45ae1cf7c4f726ed33f9dd37f418a017c3cbab
jayantkushwah8 <102745845+jayantkushwah8@users.noreply.github.com>
2023-08-02 04:45:23 +0000

    Conversion

---
+++ conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 95;
+let Score = Number(score);
+// console.log(typeof Score);
+// console.log(Score);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let name = "jayant"
+let nameConversion = Boolean(name)
+// console.log(nameConversion)
+
+let num  = "33";
+let numString = Number(num)
+console.log(numString);
